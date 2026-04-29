e71db4686b18e2367ef590244ae1686effbad177
Mohamed Tarek <koko13467955@gmail.com>
2026-04-29 04:33:02 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(tsx|ts|jsx|js)$/,
-      include: [/src/],
-      resolve: {
-        alias: {
-          'solana-drain-demo': false,
-        },
-      },
-    });
-    return config;
-  },
-  distDir: '.next',
 }
 
 module.exports = nextConfig
